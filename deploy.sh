db9f022fc9c3db375162ec2a399d46959f12c347
lucasmaurice <leptitmauricedu73@me.com>
2016-10-30 12:18:41 -0400

    Update after tests

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@ git config --global user.name "Lucas Maurice"
 git config --global user.email "lucas.maurice@outlook.com"
 ssh-keygen -t rsa -b 4096 -C "lucas.maurice@outlook.com"
 eval "$(ssh-agent -s)"
-echo "Run this command in an other shell session :"
-echo "ssh-add ~/.ssh/id_rsa"
-read -s -n1 -p "Appuyez sur une touche pour continuer..."; echo
+ssh-add ~/.ssh/id_rsa
 pbcopy < ~/.ssh/id_rsa.pub
 echo "Now go in your GitHub Settings and paste your SSH key"
