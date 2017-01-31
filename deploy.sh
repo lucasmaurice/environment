7f3dc9b1e1b33b6bf86e09699179c06afc82fc26
Lucas Maurice <lucas.maurice@outlook.com>
2017-01-31 13:46:39 -0500

    Change the method for print sha key.

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git config --global user.email "lucas.maurice@outlook.com"
 ssh-keygen -t rsa -b 4096 -C "lucas.maurice@outlook.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
-pbcopy < ~/.ssh/id_rsa.pub
-echo "Now go in your GitHub Settings and paste your SSH key."
+cat ~/.ssh/id_rsa.pub
+echo "Now go in your GitHub Settings and copy/paste your SSH key."
