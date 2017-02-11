eb7bba5eee98c286736e34d39fa3fb2aa79076dc
Lucas Maurice <lucas.maurice@outlook.com>
2017-02-11 00:22:11 -0500

    ajout de l'installation de zsh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt install zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git config --global user.name "Lucas Maurice"
 git config --global user.email "lucas.maurice@outlook.com"
 ssh-keygen -t rsa -b 4096 -C "lucas.maurice@outlook.com"
