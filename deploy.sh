1ecfe30cf795756d82f5123748a8c1c7cf1935d4
Lucas Maurice <lucas.maurice@outlook.com>
2017-02-11 10:56:33 -0500

    Ajout du source pour zsh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+#INSTALL ZSH TERMINAL
 sudo apt install zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+#INSTALL PREFERED SOFTWARES
+sudo apt install chromium-browser
+wget -P temp https://atom.io/download/deb
+sudo dpkg -i temp/deb
+rm -R temp
+
+#CONFIGURE GITHUB
 git config --global user.name "Lucas Maurice"
 git config --global user.email "lucas.maurice@outlook.com"
 ssh-keygen -t rsa -b 4096 -C "lucas.maurice@outlook.com"
