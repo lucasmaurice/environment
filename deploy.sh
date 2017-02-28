33af87d3cfa8165b1f4b84ee14f2f900a60fa91e
Lucas Maurice <lucas.maurice@outlook.com>
2017-02-28 04:53:59 -0500

    Changement d'ordre des commandes dans le fichier de deploiement pour gerer correctement l'installation de zsh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-#INSTALL ZSH TERMINAL
-sudo apt install zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
 #INSTALL PREFERED SOFTWARES
-sudo apt install chromium-browser
+sudo apt-get install chromium-browser
 wget -P temp https://atom.io/download/deb
 sudo dpkg -i temp/deb
 rm -R temp
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update
-sudo apt install spotify-client
+sudo apt-get update
+sudo apt-get install spotify-client
 
 #CONFIGURE GITHUB
 git config --global user.name "Lucas Maurice"
@@ -22,3 +18,8 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
 echo "Now go in your GitHub Settings and copy/paste your SSH key."
+
+
+#INSTALL ZSH TERMINAL
+sudo apt-get install zsh
+#sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
