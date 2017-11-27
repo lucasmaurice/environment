08a7bbe36625329b51097e957850f91f0c2a09da
Lucas Maurice <lucasmaurice@users.noreply.github.com>
2017-11-26 22:14:54 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
 sudo apt-get install spotify-client
+sudo apt-get install xclip
 
 #CONFIGURE GITHUB
 git config --global user.name "Lucas Maurice"
