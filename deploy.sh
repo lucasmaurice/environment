ef25bb4161354c59aaf8496eff1ecce3e7b66e40
Lucas Maurice <lucasmaurice@users.noreply.github.com>
2017-11-26 22:09:17 -0500

    Update deploy.sh
    Change Spotify installation script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#INSTALL PREFERED SOFTWARES
+# ATOM
 wget -P temp https://atom.io/download/deb
 sudo dpkg -i temp/deb
 rm -R temp
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+# SPOTIFY
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update
-sudo apt install spotify-client
+sudo apt-get update
+sudo apt-get install spotify-client
 
 #CONFIGURE GITHUB
 git config --global user.name "Lucas Maurice"
