cab3312c456ab764d07e253d6a96bbf9a1ece606
Lucas Maurice <lucas.maurice@outlook.com>
2018-04-15 23:21:57 -0400

    Some improvements in jetbrain installer.

--- installers/jetbrains-toolbox.sh
+++ installers/jetbrains-toolbox.sh
@@ -14,13 +14,15 @@ sudo wget -P $THE_PATH $THE_URL
 
 echo -e "${GREEN}Unzipping${NC}"
 sudo tar xfz ${THE_PATH}/${THE_ZIP} -C ${THE_PATH} --verbose
+sudo rm ${THE_PATH}/${THE_ZIP}
 
 UNZIPPED_DIR=$(ls -d $THE_PATH/*/)
 echo -e "${GREEN}DIR: ${NC} $UNZIPPED_DIR"
 echo -e "${GREEN}Fixing rights${NC}"
 
+sudo chmod 755 $UNZIPPED_DIR -R
 sudo chown $SUDO_USER:$SUDO_USER $UNZIPPED_DIR -R
 
 echo -e "${GREEN}Installing${NC}"
 
-${UNZIPPED_DIR}jetbrains-toolbox &
+${UNZIPPED_DIR}jetbrains-toolbox & echo -e "${BLUE}===== END -- Install Jetbrains Toolbox =====${NC}"
