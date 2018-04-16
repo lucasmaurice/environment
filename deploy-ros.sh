594604f2befdeaf4c388a2687d8695807f7ccdb4
Lucas Maurice <lucas.maurice@outlook.com>
2018-04-15 20:48:06 -0400

    Add auto accept for apt install.

--- deploy-ros.sh
+++ deploy-ros.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update
-sudo apt-get install ros-kinetic-desktop-full
-sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt update
+sudo apt install -y ros-kinetic-desktop-full
+sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
 rosdep update
 
@@ -11,7 +11,7 @@ echo "source /opt/ros/kinetic/setup.zsh" >> ~/.zshrc
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 
 case $SHELL in
-*/zsh) 
+*/zsh)
    # assume Zsh
    source ~/.zshrc
    ;;
