509a6cf9679abeb618f7b7ce259a8538367205f7
Lucas <lucas.maurice@outlook.com>
2019-05-23 10:35:12 -0400

    Use spaces instead of tabs.

--- deploy.sh
+++ deploy.sh
@@ -41,120 +41,120 @@ PACKAGES=(
     tree
     vim
     wget
-	zsh
-	nmap
-	htop
-	minicom
-	mtr
+    zsh
+    nmap
+    htop
+    minicom
+    mtr
 )
 
 CASKS_PACKAGES=(
     iterm2
     slack
-	caffeine
-	docker
-	transmission
-	1password
-	visual-studio-code
-	spotify
-	dockey
-	postman
-	tunnelblick
+    caffeine
+    docker
+    transmission
+    1password
+    visual-studio-code
+    spotify
+    dockey
+    postman
+    tunnelblick
 )
 
 PYTHON_PACKAGES=(
     virtualenv
     virtualenvwrapper
-	ansible
-	molecule
-	molecule[docker]
-	asn1crypto
-	bcrypt
-	cffi
-	cryptography
-	idna
-	Jinja2
-	MarkupSafe
+    ansible
+    molecule
+    molecule[docker]
+    asn1crypto
+    bcrypt
+    cffi
+    cryptography
+    idna
+    Jinja2
+    MarkupSafe
 )
 
 # Filter by system type
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	echo "${GREEN}Begin:${NC} Run deployment as Linux system."
+    echo "${GREEN}Begin:${NC} Run deployment as Linux system."
 
-	# Prepare
-	echo -e "${GREEN}Deployment:${NC} Update Aptitude packages."
-	sudo apt update -qqq
-	sudo apt-get -f install -y -qqq
-	sudo apt upgrade -y -qqq
+    # Prepare
+    echo -e "${GREEN}Deployment:${NC} Update Aptitude packages."
+    sudo apt update -qqq
+    sudo apt-get -f install -y -qqq
+    sudo apt upgrade -y -qqq
 
-	# Divers tools
-	echo -e "${GREEN}Deployment:${NC} Installing Divers Tools"
-	sudo apt install git nmap tree htop zsh tmux vim -y -qqq
+    # Divers tools
+    echo -e "${GREEN}Deployment:${NC} Installing Divers Tools"
+    sudo apt install git nmap tree htop zsh tmux vim -y -qqq
 
-	#NEXT ARE FOR SSH LOGIN CONFIGURATION ON GITHUB
-	if [ ! -f ~/.ssh/id_rsa ]; then
-		echo -e "${GREEN}Deployment:${NC} Create SSH Key for Git"
-		ssh-keygen -t rsa -b 4096 -C $GITEMAIL
-		eval "$(ssh-agent -s)"
-		ssh-add ~/.ssh/id_rsa
-	fi
+    #NEXT ARE FOR SSH LOGIN CONFIGURATION ON GITHUB
+    if [ ! -f ~/.ssh/id_rsa ]; then
+        echo -e "${GREEN}Deployment:${NC} Create SSH Key for Git"
+        ssh-keygen -t rsa -b 4096 -C $GITEMAIL
+        eval "$(ssh-agent -s)"
+        ssh-add ~/.ssh/id_rsa
+    fi
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	echo "${GREEN}Begin:${NC} Run deployment as Mac OS system."
+    echo "${GREEN}Begin:${NC} Run deployment as Mac OS system."
 
-	# Prepare
-	if test ! $(which brew); then
-		echo "${GREEN}Deployment:${NC} Installing Brew."
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	fi
+    # Prepare
+    if test ! $(which brew); then
+        echo "${GREEN}Deployment:${NC} Installing Brew."
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
 
-	# Updating Brew Packages
-	echo "${GREEN}Deployment:${NC} Updating Brew packages."
-	brew update
-	
-	# Installing MacOS Command Line Tools
-	echo -e "${GREEN}Deployment:${NC} Installing MacOS Command Line Tools"
-	xcode-select --install
+    # Updating Brew Packages
+    echo "${GREEN}Deployment:${NC} Updating Brew packages."
+    brew update
+    
+    # Installing MacOS Command Line Tools
+    echo -e "${GREEN}Deployment:${NC} Installing MacOS Command Line Tools"
+    xcode-select --install
 
-	# Accept licences
-	echo -e "${GREEN}Deployment:${NC} Accept licences"
-	sudo xcodebuild -license
+    # Accept licences
+    echo -e "${GREEN}Deployment:${NC} Accept licences"
+    sudo xcodebuild -license
 
-	# Installing GNU core utilities (those that come with OS X are outdated)
-	echo -e "${GREEN}Deployment:${NC} Installing GNU core utilities"
-	brew install coreutils
-	brew install gnu-sed
-	brew install gnu-tar
-	brew install gnu-indent
-	brew install gnu-which
+    # Installing GNU core utilities (those that come with OS X are outdated)
+    echo -e "${GREEN}Deployment:${NC} Installing GNU core utilities"
+    brew install coreutils
+    brew install gnu-sed
+    brew install gnu-tar
+    brew install gnu-indent
+    brew install gnu-which
 
-	# Divers tools
-	echo "${GREEN}Deployment:${NC} Installing Divers Tools"
-	brew install ${PACKAGES[@]}
+    # Divers tools
+    echo "${GREEN}Deployment:${NC} Installing Divers Tools"
+    brew install ${PACKAGES[@]}
 
-	# Cleaning up
-	echo "${GREEN}Deployment:${NC} Cleaning Brew"
-	brew cleanup
+    # Cleaning up
+    echo "${GREEN}Deployment:${NC} Cleaning Brew"
+    brew cleanup
 
-	Installing Cask
-	echo "${GREEN}Deployment:${NC} Installing Cask"
-	# brew install caskroom/cask
+    Installing Cask
+    echo "${GREEN}Deployment:${NC} Installing Cask"
+    # brew install caskroom/cask
 
-	# Installing Cask Apps
-	echo "${GREEN}Deployment:${NC} Installing Cask Apps"
-	brew cask install --force --require-sha ${CASKS_PACKAGES[@]}
+    # Installing Cask Apps
+    echo "${GREEN}Deployment:${NC} Installing Cask Apps"
+    brew cask install --force --require-sha ${CASKS_PACKAGES[@]}
 
-	# Installing Python Apps
-	echo "${GREEN}Deployment:${NC} Installing Python Apps"
-	pip install ${PYTHON_PACKAGES[@]}
+    # Installing Python Apps
+    echo "${GREEN}Deployment:${NC} Installing Python Apps"
+    pip install ${PYTHON_PACKAGES[@]}
 
-	#NEXT ARE FOR SSH LOGIN CONFIGURATION ON GITHUB
-	if [ ! -f ~/.ssh/id_rsa ]; then
-		echo "${GREEN}Deployment:${NC} Create SSH Key for Git"
-		ssh-keygen -t rsa -b 4096 -C $GITEMAIL
-		eval "$(ssh-agent -s)"
-		ssh-add -K ~/.ssh/id_rsa
-	fi
+    #NEXT ARE FOR SSH LOGIN CONFIGURATION ON GITHUB
+    if [ ! -f ~/.ssh/id_rsa ]; then
+        echo "${GREEN}Deployment:${NC} Create SSH Key for Git"
+        ssh-keygen -t rsa -b 4096 -C $GITEMAIL
+        eval "$(ssh-agent -s)"
+        ssh-add -K ~/.ssh/id_rsa
+    fi
 else
         echo "${YELLOW}Error:${NC} OS is not supported."
 fi
@@ -164,10 +164,10 @@ fi
 echo "${GREEN}Deployment:${NC} Installation of dotfiles in ./dotfiles"
 for f in $(ls ./dotfiles)
 do
-	echo "${YELLOW}Dotfile:${NC} $(echo $f )"
-	if [ ! -d ./dotfiles/${f} ]; then
-		cp -f ./dotfiles/${f} ~/.${f}
-	fi
+    echo "${YELLOW}Dotfile:${NC} $(echo $f )"
+    if [ ! -d ./dotfiles/${f} ]; then
+        cp -f ./dotfiles/${f} ~/.${f}
+    fi
 done
 
 # CONFIGURE GITHUB
@@ -179,7 +179,7 @@ git config --global user.email $GITEMAIL
 echo "${GREEN}Deployment:${NC} Configure ZSH"
 if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-	sudo chsh -s /bin/zsh $(whoami)
+    sudo chsh -s /bin/zsh $(whoami)
 fi
 
 # WALLPAPERS
